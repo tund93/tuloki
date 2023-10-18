a38f0e9d75e8d36fec5a5fd061dd3a3dd5117159
tund93 <nguyendanhtu789@gmail.com>
2023-10-18 15:42:08 +0700

    update font

--- apps/catalog/.storybook/preview.tsx
+++ apps/catalog/.storybook/preview.tsx
@@ -2,7 +2,7 @@ import { withThemeByDataAttribute } from '@storybook/addon-styling';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import type { Preview } from '@storybook/react';
 import { ThemeProvider } from '@tuloki/core/component/theme-provider';
-import { fontFamily } from '@tuloki/core/font/family';
+import { firaCode, getFontVariables, inter, notoSans, rakkas } from '@tuloki/core/font/family';
 import { cn } from '@tuloki/tailwind';
 import React from 'react';
 import './storybook.css';
@@ -20,7 +20,7 @@ const preview: Preview = {
     // FIXME: I'm trying to add fontFamily to className and load the font by next/font, but oddly enough this does not work correctly.
     // Probably due to the fact that it work on monorepo.
     (Story) => (
-      <div className={cn(fontFamily, 'font-sans')}>
+      <div className={cn(getFontVariables([firaCode, inter, notoSans, rakkas]), 'font-sans')}>
         <Story />
       </div>
     ),
